b4a80f7ed8d157dd94f0e53a850e7dc29cf86703
Avinash <avinashghoshpullur@gmail.com>
2021-12-07 01:12:40 +0530

    Added welcome page

---
+++ src/container/Welcome.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import WelcomeComponent from "../components/welcome";
+
+const Welcome = () => {
+
+    return (
+        <WelcomeComponent />
+    );
+}
+
+export default Welcome
