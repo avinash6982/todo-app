2cbfd11c5636fc6fdd8468ecdc84256a442299cd
Avinash <avinashghoshpullur@gmail.com>
2021-12-18 23:14:04 +0530

    Implemented mock apis for signin and signup

--- src/api/mocks/MockUserDb.js
+++ src/api/mocks/MockUserDb.js
@@ -1,1 +1,12 @@
 export const userEmails = ['user@user.com', 'user@user.in']
+
+export const userCredentials = [
+    {
+        email: 'user@user.com',
+        password: '12345user!'
+    },
+    {
+        email: 'user@user.in',
+        password: '12345user!'
+    }
+]
